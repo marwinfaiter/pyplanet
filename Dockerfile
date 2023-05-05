5558d43deab812590ccffda98b11216c8f1da836
Marwin Faiter <noobgubbe@live.se>
2023-05-05 20:50:59 +0200

    Added possibility to install apps dynamically through environment variable

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,6 @@ RUN pyplanet init_project server
 
 COPY contrib/map/manager.py /usr/local/lib/python3.8/site-packages/pyplanet/contrib/map/manager.py
 COPY settings/base.py /pyplanet/server/settings/base.py
+COPY settings/apps.py /pyplanet/server/settings/apps.py
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,28 @@
 
 pip install pyplanet --upgrade
 
+IFS=';' read -ra APPS_ARRAY <<< "$APPS"
+for app_string in "${APPS_ARRAY[@]}"
+do
+    IFS='|' read -ra URL_APPS_ARRAY <<< "$app_string"
+
+    APPS=${URL_APPS_ARRAY[0]}
+    URL=${URL_APPS_ARRAY[1]}
+
+    IFS=',' read -ra APPS <<< "$APPS"
+    for app in "${APPS[@]}"
+    do
+        if [[ ! -z "$URL" ]]
+        then
+            echo COMMAND python -m pip install -i "$URL" "$app"
+            python -m pip install -i "$URL" "$app"
+        else
+            echo COMMAND: python -m pip install "$app"
+            python -m pip install "$app"
+        fi
+
+    done
+
+done
+
 exec server/manage.py start
